15870e8caee1ba4f5a7c7fd9e99c14b6b7103ae0
Godwin <agblezeg@gmail.com>
2023-06-26 12:00:33 +0200

    Add processWeatherData function

--- index.js
+++ index.js
@@ -8,8 +8,28 @@ async function getWeatherData(location) {
   );
 
   const data = await response.json();
-  console.log(data);
-  return data;
+
+  const processedData = await processWeatherData(data);
+
+  console.log("Processed Data", processedData);
+  return processedData;
 }
 
 getWeatherData(inputLocation);
+
+async function processWeatherData(data) {
+  const weatherData = await data;
+
+  const processedData = {
+    location: weatherData.location.name,
+    country: weatherData.location.country,
+    localTime: weatherData.location.localtime,
+    condition: weatherData.current.condition.text,
+    tempCelsius: weatherData.current.temp_c,
+    tempFahrenheit: weatherData.current.temp_f,
+    windKph: weatherData.current.wind_kph,
+    windMph: weatherData.current.wind_mph,
+  };
+
+  return processedData;
+}
