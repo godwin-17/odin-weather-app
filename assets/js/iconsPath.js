11c20516dba720528fe374e27d9a489d5892ac4b
Godwin <agblezeg@gmail.com>
2023-07-01 19:25:57 +0200

    Create icon mapping

---
+++ assets/js/iconsPath.js
@@ -0,0 +1,194 @@
+export const iconMapping = {
+  1000: [
+    "./assets/weather-icons/day/1000.svg",
+    "./assets/weather-icons/night/1000.svg",
+  ],
+  1003: [
+    "./assets/weather-icons/day/1003.svg",
+    "./assets/weather-icons/night/1003.svg",
+  ],
+  1006: [
+    "./assets/weather-icons/day/1006.svg",
+    "./assets/weather-icons/night/1006.svg",
+  ],
+  1009: [
+    "./assets/weather-icons/day/1009.svg",
+    "./assets/weather-icons/night/1009.svg",
+  ],
+  1030: [
+    "./assets/weather-icons/day/1030.svg",
+    "./assets/weather-icons/night/1030.svg",
+  ],
+  1063: [
+    "./assets/weather-icons/day/1063.svg",
+    "./assets/weather-icons/night/1063.svg",
+  ],
+  1066: [
+    "./assets/weather-icons/day/1066.svg",
+    "./assets/weather-icons/night/1066.svg",
+  ],
+  1069: [
+    "./assets/weather-icons/day/1069.svg",
+    "./assets/weather-icons/night/1069.svg",
+  ],
+  1072: [
+    "./assets/weather-icons/day/1072.svg",
+    "./assets/weather-icons/night/1072.svg",
+  ],
+  1087: [
+    "./assets/weather-icons/day/1087.svg",
+    "./assets/weather-icons/night/1087.svg",
+  ],
+  1114: [
+    "./assets/weather-icons/day/1114.svg",
+    "./assets/weather-icons/night/1114.svg",
+  ],
+  1117: [
+    "./assets/weather-icons/day/1117.svg",
+    "./assets/weather-icons/night/1117.svg",
+  ],
+  1135: [
+    "./assets/weather-icons/day/1135.svg",
+    "./assets/weather-icons/night/1135.svg",
+  ],
+  1147: [
+    "./assets/weather-icons/day/1147.svg",
+    "./assets/weather-icons/night/1147.svg",
+  ],
+  1150: [
+    "./assets/weather-icons/day/1150.svg",
+    "./assets/weather-icons/night/1150.svg",
+  ],
+  1153: [
+    "./assets/weather-icons/day/1153.svg",
+    "./assets/weather-icons/night/1153.svg",
+  ],
+  1168: [
+    "./assets/weather-icons/day/1168.svg",
+    "./assets/weather-icons/night/1168.svg",
+  ],
+  1171: [
+    "./assets/weather-icons/day/1171.svg",
+    "./assets/weather-icons/night/1171.svg",
+  ],
+  1180: [
+    "./assets/weather-icons/day/1180.svg",
+    "./assets/weather-icons/night/1180.svg",
+  ],
+  1183: [
+    "./assets/weather-icons/day/1183.svg",
+    "./assets/weather-icons/night/1183.svg",
+  ],
+  1186: [
+    "./assets/weather-icons/day/1186.svg",
+    "./assets/weather-icons/night/1186.svg",
+  ],
+  1189: [
+    "./assets/weather-icons/day/1189.svg",
+    "./assets/weather-icons/night/1189.svg",
+  ],
+  1192: [
+    "./assets/weather-icons/day/1192.svg",
+    "./assets/weather-icons/night/1192.svg",
+  ],
+  1195: [
+    "./assets/weather-icons/day/1195.svg",
+    "./assets/weather-icons/night/1195.svg",
+  ],
+  1198: [
+    "./assets/weather-icons/day/1198.svg",
+    "./assets/weather-icons/night/1198.svg",
+  ],
+  1201: [
+    "./assets/weather-icons/day/1201.svg",
+    "./assets/weather-icons/night/1201.svg",
+  ],
+  1204: [
+    "./assets/weather-icons/day/1204.svg",
+    "./assets/weather-icons/night/1204.svg",
+  ],
+  1207: [
+    "./assets/weather-icons/day/1207.svg",
+    "./assets/weather-icons/night/1207.svg",
+  ],
+  1210: [
+    "./assets/weather-icons/day/1210.svg",
+    "./assets/weather-icons/night/1210.svg",
+  ],
+  1213: [
+    "./assets/weather-icons/day/1213.svg",
+    "./assets/weather-icons/night/1213.svg",
+  ],
+  1216: [
+    "./assets/weather-icons/day/1216.svg",
+    "./assets/weather-icons/night/1216.svg",
+  ],
+  1219: [
+    "./assets/weather-icons/day/1219.svg",
+    "./assets/weather-icons/night/1219.svg",
+  ],
+  1222: [
+    "./assets/weather-icons/day/1222.svg",
+    "./assets/weather-icons/night/1222.svg",
+  ],
+  1225: [
+    "./assets/weather-icons/day/1225.svg",
+    "./assets/weather-icons/night/1225.svg",
+  ],
+  1237: [
+    "./assets/weather-icons/day/1237.svg",
+    "./assets/weather-icons/night/1237.svg",
+  ],
+  1240: [
+    "./assets/weather-icons/day/1240.svg",
+    "./assets/weather-icons/night/1240.svg",
+  ],
+  1243: [
+    "./assets/weather-icons/day/1243.svg",
+    "./assets/weather-icons/night/1243.svg",
+  ],
+  1246: [
+    "./assets/weather-icons/day/1246.svg",
+    "./assets/weather-icons/night/1246.svg",
+  ],
+  1249: [
+    "./assets/weather-icons/day/1249.svg",
+    "./assets/weather-icons/night/1249.svg",
+  ],
+  1252: [
+    "./assets/weather-icons/day/1252.svg",
+    "./assets/weather-icons/night/1252.svg",
+  ],
+  1255: [
+    "./assets/weather-icons/day/1255.svg",
+    "./assets/weather-icons/night/1255.svg",
+  ],
+  1258: [
+    "./assets/weather-icons/day/1258.svg",
+    "./assets/weather-icons/night/1258.svg",
+  ],
+  1261: [
+    "./assets/weather-icons/day/1261.svg",
+    "./assets/weather-icons/night/1261.svg",
+  ],
+  1264: [
+    "./assets/weather-icons/day/1264.svg",
+    "./assets/weather-icons/night/1264.svg",
+  ],
+  1273: [
+    "./assets/weather-icons/day/1273.svg",
+    "./assets/weather-icons/night/1273.svg",
+  ],
+  1276: [
+    "./assets/weather-icons/day/1276.svg",
+    "./assets/weather-icons/night/1276.svg",
+  ],
+  1279: [
+    "./assets/weather-icons/day/1279.svg",
+    "./assets/weather-icons/night/1279.svg",
+  ],
+  1282: [
+    "./assets/weather-icons/day/1282.svg",
+    "./assets/weather-icons/night/1282.svg",
+  ],
+};
